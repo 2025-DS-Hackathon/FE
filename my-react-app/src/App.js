5fb320438c59d09c7b7605efab015c574e003418
20240787 <20240787@duksung.ac.kr>
2025-11-19 14:50:29 +0900

    main페이지 수정 및 notification창 제작

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // BrowserRouter를 사용합니다.
 import TargetPage from './pages/Talent.js'; 
 import Login from "./pages/Login.jsx";
-import Main from "./pages/Main.js";
+import Main from "./pages/Main.jsx";
 import ExtraInfo from "./pages/ExtraInfo.jsx";
 import MypageGuest from "./pages/MypageGuest";
 import MypageUser from "./pages/MypageUser";
+import Notification from "./pages/Notification";
+
 
 function App() {
   return (
@@ -14,16 +16,16 @@ function App() {
 
         <Route path="/login" element={<Login />} /> 
         <Route path="/extra" element={<ExtraInfo />} />
-<<<<<<< HEAD
+
         {/* 메인 페이지 */}
         <Route path="/main" element={<Main />} />
         <Route path="/mypage" element={<MypageGuest />} />
         <Route path="/mypage-user" element={<MypageUser />} />
 
-=======
         <Route path="/main" element={<Main />} /> 
         <Route path="/target" element={<TargetPage />} />
->>>>>>> main
+        <Route path="/notifications" element={<Notification />} />
+
       </Routes>
 
     </Router>
